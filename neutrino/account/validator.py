b816289ac43d747710c65685f26ab4093024b514
root <root@Parsa>
2025-05-24 08:22:15 -0400

    Fix validator from account

--- neutrino/account/validator.py
+++ neutrino/account/validator.py
@@ -1,39 +1,45 @@
-from django.core.checks import messages
 from django.core.validators import(
 EmailValidator ,
 RegexValidator ,
-
 )
-
+from django.core.exceptions import ValidationError
 from django.core.validators import RegexValidator , EmailValidator
 from django.db import models
 
-from abc import abstractmethod
 class BaseValidtorMixinCore:
     regex = ...
     def __init__(self , message:str|None=None , code :str = None):
-        if message : self.message = message
-        if code : self.code = code
+        self.message = message if message else None
+        self.code = code if code else None
     def __call__(self , values):
         self.check_validation(values)
-    
+
     def check_validation(self , values):
-        values_validator = RegexValidator(regex=FileValidatoPentrate.regex) 
+        values_validator = RegexValidator(regex=self.regex , message=self.message,code=self.code) 
         values_validator(values)
 
-from rest_framework.serializers import Serializer
 
 class FileValidatoPentrate(
     BaseValidtorMixinCore
 ):
     regex = r"^[a-zA-Z0-9_-]+\.(jpg|jpeg|png|gif|webp|mp4|mov|avi|mkv|mp3|wav|flac|pdf|docx|txt|zip|rar)$"
 
-
+# is not workning 
 class PhoneValidator(BaseValidtorMixinCore):
     regex = r"^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$"
 
 class SqInjectionSafeValidator(BaseValidtorMixinCore):
     regex = r'^[^"\';<>]*$'
+
+class SqInjectionSafeValidatorPassword(BaseValidtorMixinCore):
+    regex = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$'
+
+
+
+
+
+def no_Empty_validator(values):
+    assert values , ValidationError("Empty Error Values")
 
 class ValidationsAccount:
     username_validation = RegexValidator(
@@ -67,3 +73,10 @@ class ValidationsAccount:
         """ , 
         code = "sql_injection" 
     )
+    sql_injection_safe_validator_password = SqInjectionSafeValidatorPassword(
+        message = """
+        not valid this is for injection 
+        """ , 
+        code = "sql_injection" 
+    )
+    NotEmpty = no_Empty_validator
