31244f69c53a84c3c5c0c43a3df3b09c0a1be3cb
overdoseparsa <parsakhakiy@gmail.com>
2025-07-01 16:10:44 +0330

    adding post app to the settigns

--- config/django/base.py
+++ config/django/base.py
@@ -20,6 +20,7 @@ LOCAL_APPS = [
     'neutrino.authentication.apps.AuthenticationConfig',
     'neutrino.account.apps.AccountConfig' , 
     'neutrino.otp.apps.OtpConfig' , 
+    'neutrino.post.apps.Postconfig' , 
 ]
 
 THIRD_PARTY_APPS = [
