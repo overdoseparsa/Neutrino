7ef2051a1a69f0d51bf0bf98000a0f243ef5c732
overdoseparsa <parsakhakiy@gmail.com>
2025-07-22 09:15:58 -0400

    adding the XssSecurityProvider

--- neutrino/core/provider.py
+++ neutrino/core/provider.py
@@ -1,82 +1,46 @@
-class BaseRequestValidator():
+from abc import ABC , abstractmethod
+class BaseRequestValidator(ABC):
     def __init__(self , request):
-        print('this is from requst limitation')
+        self._request = request
+        self.configure()
 
+    @abstractmethod
+    def configure(self):
+        "..."
+
+        
 
 class RequestsLimitationProvider(BaseRequestValidator):
     def __init__(self, request):
         super().__init__(request)
 
 
+from django.core.validators import RegexValidator
+import re 
+# HINT : dependency inversion princile  
 
-from django.contrib.auth import get_user_model
-from django.http import HttpRequest
-from django.utils import timezone
-import datetime
-
-user = get_user_model()
-
-
-def Create_user(data): 
-    return user.objects.create_user(
-        username=data.get('username') , 
-        email= data.get('email') , 
-        password= data.get('password')
-    )
-
-
-# this is base Create Data
-# def _check_validation_time_session(time_block_utils):
-#     return  is_time_difference_less(time_block_utils , timezone.now() , datetime.timedelta(hours=3)  )
-    
-class UserCreateBase: # rate limite 
-    'logic may be scable but is is the base simple logic'
-    # redis 
-    def __init__(self , data , request:HttpRequest , **kwargs) ->None:
-        self.data = data   
-        self._request = request 
-        self.validate_request_session()
-    
-    def validate_request_session(self)->None: 
-        '''This is important who try to login in our services'''
-        if not self._request.session.session_key:  # TODO use redis here for better permfomance 
-                self._request.session.create()
-
-                self._request.session['pre_login_tracking'] = {
-                    'ip': self._request.META.get('REMOTE_ADDR'),
-                    'user_agent': self._request.META.get('HTTP_USER_AGENT'),
-                    'first_seen': str(timezone.now()),
-                    'last_time':None , 
-                    'login_attempts': 0 ,
-                    'is_prime_block':False , 
-                    'time_block_utils':None
-                }
-
+class ValidateBase(ABC):
+    regex = r'(<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>)'
         
-        # else : 
-        #     if self._request.session['pre_login_tracking']['is_prime_block']:
-        #         if _check_validation_time_session(self._request.session['pre_login_tracking']['is_prime_block']):
-        #             raise TimeSessionExpection('That attemped from This session time is not expired \' try later')  
-        #         else : 
-        #             self._request.session['pre_login_tracking']['is_prime_block'] = False
-        #             self._request.session['pre_login_tracking']['time_block_utils'] = None                                              
-        #     else :
-        #         self.attempt = self._request.session['pre_login_tracking']
+    @abstractmethod
+    def validate(self):
+        """validation here"""
+
+class RegexDjangovalidation():
+
+        # dependency injection 
+    def __init__(self):
+        self.__regex_validaor = RegexValidator(regex=self.regex , message="malicious scripts Xss revrence" , code="403" ,flags="Forbidden" )
+        
+    def validate(self, content):
+        self.__regex_validaor(content)
+    
 
 
-
-    def servicce_login():
-        pass
-
-
-
-
-
-# check session from server [*]
-# using otp from server 
-# validate otp 
-# after validate create the user and return authtaction from JWT 
-# after that send the emeil that u are loggin in to the this service 
-# it is better to just use email for varifacation we can use phone 
-      
-
+class XssSecurityProvider(BaseRequestValidator): 
+    # dependency injection 
+    def configure(self):
+        Regex_obj = RegexDjangovalidation()
+        for key, value in self.request.POST.items():
+            Regex_obj.validate(value)
+  
